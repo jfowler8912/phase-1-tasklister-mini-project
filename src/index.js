34dc77abc146bb79c0d38286ea982db469e6b44c
JoshuaFowler <jfowler8912@gmail.com>
2023-03-23 16:51:50 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // Get form
+  const form = document.querySelector('#create-task-form')
+  // Display area for tasks
+  const taskList = document.getElementById('tasks')
+
+  // Store value that was put into the field
+  function addTask(task) {
+    const li = document.createElement('li');
+    li.innerText = task
+    taskList.appendChild(li)
+  }
+  
+  // Press submit button to add to to-do list
+  // Display value in new list on page
+  form.addEventListener('submit', (e) =>
+  {
+    e.preventDefault()
+    //console.log(form.new-task-description.value)
+    addTask(form.newtaskdescription.value)
+  })
 });
+
+
+
+
+
+
